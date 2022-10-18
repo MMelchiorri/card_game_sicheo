50de50c5ae73a08140b59f2727346eba74655c6a
Gatsufy <mm.melchiorri@gmail.com>
2022-10-18 11:10:05 +0200

    removed game and player routes

--- backend/src/Routes/GameRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as controller from '../Controller/GameController'
-
-const router_game = Router()
-
-router_game.route('/').get(controller.play_a_game)
-
-export default router_game
--- backend/src/Routes/PlayerRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as controller from '../Controller/PlayerController'
-
-const router_player = Router()
-
-router_player.route('/').get(controller.player_turn)
-
-export default router_player
